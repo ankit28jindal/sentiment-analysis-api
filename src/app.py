3bacf637ea7f75fe0bf3db01bb30a2dd73837428
Ankit Jindal <ankit28jindal@gmail.com>
2018-10-14 17:39:27 -0700

    initial commit

---
+++ src/app.py
@@ -0,0 +1,62 @@
+import os
+import json
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import tweepy
+from textblob import TextBlob
+
+
+with open(os.environ['config_file_path'], 'r') as file:
+    config: dict = json.load(file)
+
+print(config)
+
+auth = tweepy.OAuthHandler(config['Twitter']['consumer_key'], config['Twitter']['consumer_secret'])
+auth.set_access_token(config['Twitter']['access_token'], config['Twitter']['access_token_secret'])
+t_api = tweepy.API(auth)
+
+app = Flask(__name__)
+api = Api(app)
+
+class User(Resource):
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Access-Control-Allow-Origin, Access-Control-Allow-Methods')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+        return response
+
+    def get(self, name, number_of_tweets):
+        polarity: str
+        ptweets: list = []
+        neutweets: list = []
+        negtweets: list = []
+
+        result = tweepy.Cursor(t_api.search, q=name, lang='en').items(number_of_tweets)
+        d: list = []
+        for r in result:
+            analysis = TextBlob(r.text)
+            if analysis.polarity > 0:
+                ptweets.append(r.text)
+            elif analysis.polarity == 0:
+                neutweets.append(r.text)
+            else:
+                negtweets.append(r.text)
+
+            
+        d.append({
+            'Positive': ptweets,
+            'Neutral': neutweets,
+            'Negative': negtweets
+        })
+        return {
+            "Positive Tweets Percentage": (100*len(ptweets)/number_of_tweets),
+            "Neutral Tweets Percentage": (100*len(neutweets)/number_of_tweets),
+            "Negative Tweets Percentage": (100*len(negtweets)/number_of_tweets),
+            "Tweets": d
+        }
+
+
+api.add_resource(User, '/user/<string:name>/<int:number_of_tweets>')
+app.run()
